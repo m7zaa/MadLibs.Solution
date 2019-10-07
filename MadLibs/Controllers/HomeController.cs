e8c97dc6305c89fb5df4363559d5bac318594577
Student <example@example.com>
2019-10-07 14:32:07 -0700

    add a form for puppy madlib

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -7,8 +7,12 @@ namespace MadLibs.Controllers
     public class HomeController : Controller
     {
 
-        
         [Route("/")]
+        public ActionResult Index() { return View(); }
+
+
+
+        [Route("/form")]
         public ActionResult Form() { return View(); }
 
 
